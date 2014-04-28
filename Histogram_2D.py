ac6671044d6017c7717797ffd4b5f2914784fe6a
Alberto Cereser <mannaro85@gmail.com>
2014-04-29 00:39:29 +0200

    Code to plot the intensity histogram of a fits
    Runs on Ubuntu

--- Histogram_2D.py
+++ Histogram_2D.py
@@ -1,11 +1,21 @@
+# AC, 28 April 2014
+# DTU Fysik, alcer@fysik.dtu.dk
+# The code below plots the intensity histogram for a fits image file
+# !!! RUNS ON UBUNTU !!!
+
 import pyfits
 import numpy as np
 from matplotlib import pyplot as plt
 from skimage import viewer
 from skimage.viewer.plugins import lineprofile
+import pylab as pl
 
-img = pyfits.open('/Users/Alberto/Desktop/Test_image_raw.fits')
-#plt.hist(img,256,[0,256]); plt.show()
-new_viewer = viewer.ImageViewer(img)
-new_viewer += lineprofile.LineProfile()
-new_viewer.show()
+file = pyfits.open('/home/albus/Downloads/Test_image_raw.fits')
+img = file[0].data
+plt.hist(img,256,[0,256])
+plt.hist(img, bins=np.logspace(0.1,2,50))
+plt.yscale('log', nonposy='clip')
+plt.show()
+#new_viewer = viewer.ImageViewer(img)
+#new_viewer += lineprofile.LineProfile()
+#new_viewer.show()
