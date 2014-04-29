ef4ab1b368e58246d78b89b6e23fa4ffbb246f6f
Alberto Cereser <mannaro85@gmail.com>
2014-04-29 11:30:26 +0200

    Plots a section of the intensity histogram
    Aim: isolate extinction spots

---
+++ Plot_intensity_interval.py
@@ -0,0 +1,25 @@
+# AC, 28 April 2014
+# DTU Fysik, alcer@fysik.dtu.dk
+# This script selects a section of the intensity histogram and plots it
+# histogram found using Histogram_2D.py
+
+import pyfits
+import numpy as np
+from numpy import *
+from matplotlib import pyplot as plt
+from skimage import viewer
+from skimage.viewer.plugins import lineprofile
+import pylab as pl
+
+file = pyfits.open('/Users/Alberto/Desktop/Test_image_raw.fits')
+img = file[0].data
+size = 512
+img_filtered = ndarray((size, size), int)
+for i in range(0,size):
+  for j in range(0, size):
+    if img[i,j] in range(45, 150):
+      img_filtered[i,j] = img[i,j]
+    else:
+      img_filtered[i,j] = 0
+plt.imshow(img_filtered)
+plt.show()
